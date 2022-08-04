e1f6219968db8d03871cbf1a25548db15480a032
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-08-04 10:24:44 -0400

    Add search bar component

---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import {
+  Button,
+  Input,
+  InputGroup,
+  InputRightElement
+} from '@chakra-ui/react';
+
+export default function SearchBar({ onSubmit }) {
+  return (
+    <InputGroup
+      width={{ base: "75%", md: "50%" }}
+      margin="auto"
+    >
+      <Input placeholder="Enter a city or zip code" />
+      <InputRightElement width="min-content">
+        <Button
+          fontSize={"sm"}
+          fontWeight={600}
+          color={"white"}
+          bg={"green.400"}
+          href={"#"}
+          _hover={{ bg: "green.300", }}
+          onClick={onSubmit}>
+          Search
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  )
+}
