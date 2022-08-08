ee02165e993629c010c9c19e4221f11f383f5cf8
Safwan Islam <saislam@davidson.edu>
2022-08-07 20:59:09 -0500

    refactored to make work again

--- client/src/components/CoffeeShopCard.js
+++ client/src/components/CoffeeShopCard.js
@@ -1,11 +1,9 @@
 import {
-  Flex,
-  Spacer,
+  HStack,
   Box,
   Image,
   Heading,
   Link,
-  Text,
   useColorModeValue,
 } from '@chakra-ui/react';
 import { StarIcon } from '@chakra-ui/icons';
@@ -16,7 +14,7 @@ import { FAVORITE } from '../utils/mutations';
 export default function CoffeeShopCard({ coffeeShopData, type }) {
   const toggleTextColor = useColorModeValue('gray.600', 'gray.400');
   const [favorite, { error }] = useMutation(FAVORITE);
-  const { id, rating, review_count, location, name, url, image_url } = coffeeShopData;
+  const { id, rating, review_count, name, url, image_url } = coffeeShopData;
 
   const handleFavorite = async (id) => {
     if (!error) {
@@ -38,31 +36,28 @@ export default function CoffeeShopCard({ coffeeShopData, type }) {
   }
 
   return (
-    <Flex
-      w={{ base: '95vw', md: '65vw', lg: '30vw' }}
-      h={{ base: '125px', md: '175px' }}
+    <HStack
+      maxW='lg'
+      maxHeight={{ base: '125px', md: '175px' }}
       borderWidth='1px'
       borderRadius='lg'
       overflow='hidden'
+      paddingRight='12px'
       marginBottom='12px'
     >
       <Image
         src={image_url}
         alt={name}
         objectFit='cover'
-        minW={{ base: '125px', md: '175px' }}
-        maxW={{ base: '125px', md: '175px' }}
-        h='auto'
+        height={{ base: '125px', md: '175px' }}
+        width={{ base: '125px', md: '175px' }}
       />
       <Box p={{ base: 1, md: 4 }}>
         <Link href={url} isExternal>
-          <Heading size={{ base: 'sm', md: 'md' }} noOfLines={2}>
-            {name}
+          <Heading size={{ base: 'md', md: 'lg' }} noOfLines={2}>            
+          {name}
           </Heading>
         </Link>
-        <Text>
-          {location.display_address.join(' ')}
-        </Text>
         <Box display='flex' mt='2' alignItems='center'>
           {Array(5)
             .fill('')
@@ -73,12 +68,11 @@ export default function CoffeeShopCard({ coffeeShopData, type }) {
               />
             ))}
           <Box as='span' ml='2' color={toggleTextColor} fontSize='sm'>
-            {review_count}&nbsp;{review_count > 0 ? 'reviews' : 'review'}
+          {review_count}&nbsp;reviews
           </Box>
         </Box>
       </Box>
-      <Spacer />
-      <FavoritesButton type={'add'} onClick={() => handleFavorite(id)} />
-    </Flex>
+      <FavoritesButton type={type} onClick={() => handleFavorite(id)}/>
+    </HStack>
   );
 }
